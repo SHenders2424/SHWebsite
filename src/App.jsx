8bb294640cde594c521d06e90772ec70932ea676
SHenders2424 <s.henderson2424@gmail.com>
2025-02-28 22:08:01 -0600

    Formatting changes

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,15 @@ function App() {
       {/* Sidebar */}
       <aside className="sidebar">
         <img src={profilepic} alt="Profile" className="profile-img" />
+        <main>
+          <h2>Spencer Henderson</h2>
+          <p>
+            BS in Computer Science and Minor in Mathematics
+          </p>
+          <p>
+            University of Missouri
+          </p>
+        </main>
         <nav>
           <ul>
             <li><a href="mailto:s.henderson2424@gmail.com">Email</a></li>
@@ -26,7 +35,7 @@ function App() {
           <p>My name is Spencer Henderson and I am a Computer Science student at the University of Missouri.
             I am an undergraduate research assistant with the CERI Center, and I am working on a project with the
             Information Experience Laboratory. In this project, I develop a VR experience for neurodivergent 
-            individuals using Unity and C#. Outside of my research, I like to develop games and create websites and apps.
+            individuals using Unity and C#. Outside of my research and studies, I like to develop games and apps and create websites.
           </p>
         </header>
         <main>
