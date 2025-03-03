73afeb1ac4332834588732a8a7f005d594678bc9
SHenders2424 <s.henderson2424@gmail.com>
2025-03-03 14:21:10 -0600

    Added research section and centered resume

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,8 @@ function App() {
         <header className="App-header">
           <h1>About</h1>
           <p>My name is Spencer Henderson and I am a Computer Science student at the University of Missouri.
-            I am also working on getting a minor in Mathematics.  I participate in 
+            I am also working on getting a minor in Mathematics.  I participate in the Mizzou Computing Association, intermural sports,
+            and many game jams and hackathons throughout the year. 
             I am an undergraduate research assistant with the CERI Center, and I am working on a project with the
             Information Experience Laboratory. In this project, called USucceed, I develop a VR experience for neurodivergent 
             individuals using Unity and C#. Outside of my research and studies, I like to develop games and apps and create websites.
@@ -53,7 +54,11 @@ function App() {
         <main>
           <h1>Research</h1>
           <h2>USucceed</h2>
-          <p>My work on the USucceed project 
+          <p>My work on the USucceed project involves creating an immersive world in Unity that can be navigated in VR, developing engaging
+            minigames and curriculum, and integrating an AI pedagogical agent to enhance the experience. I work alongside neurodivergent adults
+            to create a supportive learning environment, ensuring users can engage effectively and gain valuable skills in cybersecurity. 
+            Some of the main aspects of the projectthat I have played a major role in creating are the minigames in each unit, the navigation and
+            menu system, and shaping the overall game loop to create a cohesive and interactive experience.
           </p>
         </main>
       </div>
